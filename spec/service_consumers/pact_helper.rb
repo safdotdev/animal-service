444cb63860b66ff85b1a6a8c92d7c837df5dbd07
Beth Skurrie <beth@bethesque.com>
2018-06-09 20:02:09 +1000

    feat: update verification config

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -5,7 +5,9 @@ require "./spec/service_consumers/provider_states_for_zoo_app"
 Pact.service_provider 'Animal Service' do
 
   honours_pact_with "Zoo App" do
-    pact_uri '../zoo-app/spec/pacts/zoo_app-animal_service.json'
+    pact_uri 'https://test.pact.dius.com.au/pacts/provider/Animal%20Service/consumer/Zoo%20App/latest', { username: "dXfltyFMgNOFZAxr8io9wJ37iUpY42M", password: "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"}
+    publish_verification_results ENV['CI'] == 'true'
+    app_version `git rev-parse --verify HEAD`.strip
+    app_version_tags [`git rev-parse --abbrev-ref HEAD`.strip]
   end
-
 end
