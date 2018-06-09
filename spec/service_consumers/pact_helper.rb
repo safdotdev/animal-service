e2509bd66855a76413859af1b7523da3d0415575
Beth Skurrie <beth@bethesque.com>
2018-06-09 22:49:51 +1000

    chore: use travis env vars to get commit and branch

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -7,7 +7,7 @@ Pact.service_provider 'Animal Service' do
   honours_pact_with "Zoo App" do
     pact_uri 'https://test.pact.dius.com.au/pacts/provider/Animal%20Service/consumer/Zoo%20App/latest', { username: "dXfltyFMgNOFZAxr8io9wJ37iUpY42M", password: "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"}
     publish_verification_results ENV['CI'] == 'true'
-    app_version `git rev-parse --verify HEAD`.strip
-    app_version_tags [`git rev-parse --abbrev-ref HEAD`.strip]
+    app_version ENV['TRAVIS_COMMIT'] || `git rev-parse --verify HEAD`.strip
+    app_version_tags [ ENV['TRAVIS_BRANCH'] || `git rev-parse --abbrev-ref HEAD`.strip]
   end
 end
