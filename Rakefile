82ac660ea57d7e6b615b6745079c381c39cd3c0e
Beth Skurrie <beth@bethesque.com>
2018-06-10 14:07:59 +1000

    test: add task to verify pact in local broker

--- Rakefile
+++ Rakefile
@@ -3,3 +3,7 @@ $: << File.join(File.dirname(__FILE__), "lib")
 require 'pact/tasks'
 
 task :default => 'pact:verify'
+
+Pact::VerificationTask.new(:local) do | task |
+  task.uri 'http://localhost:9292/pacts/provider/Animal%20Service/consumer/Zoo%20App/latest/master'
+end
--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -5,7 +5,7 @@ require "./spec/service_consumers/provider_states_for_zoo_app"
 Pact.service_provider 'Animal Service' do
 
   honours_pact_with "Zoo App" do
-    pact_uri 'https://test.pact.dius.com.au/pacts/provider/Animal%20Service/consumer/Zoo%20App/latest', { username: "dXfltyFMgNOFZAxr8io9wJ37iUpY42M", password: "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"}
+    pact_uri 'https://test.pact.dius.com.au/pacts/provider/Animal%20Service/consumer/Zoo%20App/latest/master', { username: "dXfltyFMgNOFZAxr8io9wJ37iUpY42M", password: "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"}
     publish_verification_results ENV['CI'] == 'true'
     app_version ENV['TRAVIS_COMMIT'] || `git rev-parse --verify HEAD`.strip
     app_version_tags [ ENV['TRAVIS_BRANCH'] || `git rev-parse --abbrev-ref HEAD`.strip]
